2b0116dc75aca9004ca44b595d32e57a1bdf8467
Ryusei Ishida <serys9469.japan@gmail.com>
2022-07-15 18:04:23 +0900

    Create setup-ubuntu.sh

---
+++ Python/setup-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "このシェルスクリプトは $ source setup.sh で実行してください"
+
+# INSTALL PYENV
+sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
+sudo apt install -y git
+git clone https://github.com/pyenv/pyenv.git
+echo 'export PYENV_ROOT="$HOME/pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv -v
+
+# INSTALL PYTHON3.9 WITH PYTNV
+$HOME/ryusei-ishida/pyenv/bin/pyenv install 3.9.8
+pyenv install 3.9.8
+pyenv versions
+pyenv global 3.9.
+
+# INSTALL POETRY
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+export PATH="$HOME/.poetry/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+poetry config virtualenvs.in-project
