1c6abeb1b375fc0e7ff171265b9c7b358cf00232
Ryusei Ishida <dev.ryusei@gmail.com>
2023-03-13 20:30:50 +0900

    Update setup_ubuntu.sh

--- Python/setup_ubuntu.sh
+++ Python/setup_ubuntu.sh
@@ -2,9 +2,11 @@
 echo "このシェルスクリプトは $ source setup.sh で実行してください"
 
 # INSTALL PYENV
-sudo apt install -y build-essential libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
-sudo apt install -y git
+sudo apt install -y update
+sudo apt install -y git git curl build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl
+
 git clone https://github.com/pyenv/pyenv.git
+
 echo 'export PYENV_ROOT="$HOME/pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
@@ -13,12 +15,12 @@ pyenv -v
 
 # INSTALL PYTHON3.9 WITH PYENV
 $HOME/ryusei-ishida/pyenv/bin/pyenv install 3.9.8
-pyenv install 3.9.8
+pyenv install 3.9.12
 pyenv versions
-pyenv global 3.9.
+pyenv global 3.9.12
 
 # INSTALL POETRY
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-export PATH="$HOME/.poetry/bin:$PATH" >> ~/.bashrc
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 poetry config virtualenvs.in-project
