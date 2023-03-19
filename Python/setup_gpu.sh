e5a9f4253558d98f92ccaf8f11e0c23c3e23025e
Ryusei Ishida <dev.ryusei@gmail.com>
2023-03-20 02:34:08 +0900

    Update setup_gpu.sh

--- Python/setup_gpu.sh
+++ Python/setup_gpu.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 # Install gpu driver
+# If you get a GPG key error
+# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install -y ubuntu-drivers-common
 sudo apt install -y nvidia-driver-470
 sudo nvidia-smi
-
-# If you get a GPG key error
-
-# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
-# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-# sudo apt update
-# sudo apt install -y ubuntu-drivers-common
-# sudo apt install -y nvidia-driver-470
-# sudo nvidia-smi
 
 # Install CUDA
 sudo apt install -y nvidia-cuda-toolkit
