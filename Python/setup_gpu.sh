1979be4523205c0ee59f9cec22ae8db7387ba6a7
Ryusei Ishida <dev.ryusei@gmail.com>
2023-04-04 15:36:31 +0900

    Update setup_gpu.sh

--- Python/setup_gpu.sh
+++ Python/setup_gpu.sh
@@ -6,7 +6,7 @@
 # sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo apt update
 sudo apt install -y ubuntu-drivers-common
-sudo ubuntu-drivers devices
+sudo ubuntu-drivers install
 
 # Install CUDA
 sudo apt install -y nvidia-cuda-toolkit
